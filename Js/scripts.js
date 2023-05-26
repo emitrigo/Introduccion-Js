96289316440a21fee34d4d060a137a87a76171ee
emitrigo <emi_trigo@hotmail.com>
2023-05-25 21:03:32 -0300

    introducción Js

---
+++ Js/scripts.js
@@ -0,0 +1,36 @@
+/*alert("Hola Mundo")
+alert("Que lindo es la programación")
+prompt("Te gusta programar?")
+*/
+
+
+
+// Etiquetas let, var y const
+
+let nombre = "Emiliano";
+console.log(nombre);
+
+var edad = 29;
+console.log(edad);
+
+const mascota = "Lola";
+console.log(mascota);
+
+//LET: Podemos cambiarle el dato pero podemos NO REDECLARARLO
+//let nombre = "Chiara";
+nombre= "Chiara"
+console.log(nombre)
+
+//VAR: Podemos redeclarar y cambiar un dato nuevo por el anterior
+var edad = 22
+console.log(edad);
+
+// CONST: No nos permite REDECLARAR ni REASIGNAR un valor a una variable, es decir que no se puede cambiar el nombre de Lola en este caso
+mascota = "Candela";
+console.log(mascota);
+
+
+
+
+
+
